0c0ee16efda6fc66bdadf864f4118ca16cf19f57
sabumkevin <142266141+sabumkevin@users.noreply.github.com>
2024-12-05 12:35:16 -0800

    Add files via upload
    my assignment

---
+++ DataTypeExample.java
@@ -0,0 +1,18 @@
+public class DataTypeExample {
+    public static void main(String[]arge){
+        int age = 25;
+        boolean isActive = true;
+        String name = "Nadine";
+        System.out.println("Name:" + name + "Age:" + age + ",Active:" + isActive);
+
+        int[] numbers = {1,2,3,4,5};
+    for(int num : numbers ){
+       System.out.println(num);
+
+      
+    }
+    }
+ }
+
+     
+
